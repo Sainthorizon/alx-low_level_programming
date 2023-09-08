7472ee0c0c066cc2d33f9e20a90e00a14c70f710
sainthorizon <okekeeugene14b@gmail.com>
2023-09-07 22:02:35 -0700

    correect

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - this is my main function
- *
- * Return: always 0
+ * main - this is the main put function
+ * Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+				return (0);
 }
+
 
