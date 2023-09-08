87d9aa3fab39504ebef0fafcd7db967915c51f5d
sainthorizon <okekeeugene14b@gmail.com>
2023-09-07 21:52:53 -0700

    my first betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is my main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
+
