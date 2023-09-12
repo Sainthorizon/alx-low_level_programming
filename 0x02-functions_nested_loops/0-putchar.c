d5e7c1d2156fa28b9fb6bac4d95b14d9dce874a2
sainthorizon <okekeeugene14b@gmail.com>
2023-09-11 23:29:48 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of a program
+ *
+ * Description: this function prints _putcha
+ * r, followed by a new line.
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
