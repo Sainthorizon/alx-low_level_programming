6c97461f3983dce5f8bf56d0a561cfb23242b7b0
sainthorizon <okekeeugene14b@gmail.com>
2023-09-21 13:59:14 -0700

    moore

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 include "main.h"
+
 /**
  * _strncpy - copy a string
  * @dest: input value
@@ -7,10 +8,10 @@ include "main.h"
  *
  * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
-
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
